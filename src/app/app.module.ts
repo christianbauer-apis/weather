70365e9cfb9a8fdf4e1fc5921102dcb2b6c6f63e
christianbauer-apis <christian-bauer@hotmail.com>
2020-04-22 19:11:13 -0500

    weather app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,87 @@
+/*
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UvComponent } from './uv/uv.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UvComponent,
+    CurrentWeatherComponent,
+    ForecastComponent,
+    HomePageComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+*/
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatInputModule} from '@angular/material/input';
+import { MatTabsModule} from '@angular/material/tabs';
+import { MatCardModule} from '@angular/material/card';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatListModule} from '@angular/material/list';
+
+import { HomePageComponent } from './home-page/home-page.component';
+import { StoreModule } from '@ngrx/store';
+import { locationReducer } from './location-reducer';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { FormsModule } from '@angular/forms';
+import { WeatherService } from './weather.service';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { UvComponent } from './uv/uv.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    TopBarComponent,
+    CurrentWeatherComponent,
+    ForecastComponent,
+    UvComponent,
+    AlertsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    StoreModule.forRoot({
+      loc: locationReducer
+    }),
+    FormsModule,
+    MatInputModule,
+    MatTabsModule,
+    MatCardModule,
+    HttpClientModule,
+    MatDividerModule,
+    MatListModule
+  ],
+  providers: [
+    WeatherService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
